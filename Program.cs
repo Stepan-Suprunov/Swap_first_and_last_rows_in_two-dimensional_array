d99470db8af74cf218f9e72beced67ab4f3df5d5
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-07-08 23:42:09 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// Поменять местами первую и последнюю строки в двумерном массиве
+
+
+int m = 3, n = 4;
+int[,] array = new int[m,n];
+
+void PrintArray(int[,] collection)
+{
+    for (int i = 0; i < collection.GetLength(0); i++)
+    {
+        for (int j = 0; j < collection.GetLength(1); j++)
+        {
+            Console.Write(collection[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] collection)
+{
+    for (int i = 0; i < collection.GetLength(0); i++)
+    {
+        for (int j = 0; j < collection.GetLength(1); j++)
+        {
+            collection[i,j] = new Random().Next(-99,100);
+        }
+    }
+}
+
+void ChangeRaws(int[,] collection)
+{
+    int mem;
+    for (int i = 0; i <= 0; i++)
+    {
+        for (int j = 0; j < collection.GetLength(1); j++)
+        {
+            mem = collection[collection.GetLength(0)-1,j];
+            collection[collection.GetLength(0)-1,j] = collection[i,j];
+            collection[i,j] = mem;
+        }
+    }
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("_______________");
+ChangeRaws(array);
+PrintArray(array);
